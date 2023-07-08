57520f199fa0abd3f024fed64623dde54c9a688c
Paulo César Conduru <133069658+pcconduru@users.noreply.github.com>
2023-07-08 18:01:09 -0300

    Create ex013.py

---
+++ Exercicios/ex013.py
@@ -0,0 +1,4 @@
+#Reajuste Salárial
+salário = float(input('Qual o salário do funcionário: R$'))
+aumento = (salário * 15) / 100
+print('O sálario do funcionário que é de R${} mais o aumento dos 15% se tornará R${}'.format(salário, salário+aumento))
