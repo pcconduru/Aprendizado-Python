848ca74cbd1aa673ff0f6c8a5149d891145a4cbf
Paulo César Conduru <133069658+pcconduru@users.noreply.github.com>
2023-07-08 15:20:20 -0300

    Create ex008.py

---
+++ Exercicios/ex008.py
@@ -0,0 +1,6 @@
+m = float(input('Escolha um valor a ser convertido: '))
+c = m * 100
+ml = c * 100
+print('Valor escolhido em metros: ', m)
+print('Valor convertido em centímetros: ', c)
+print('Valor convertido em milimetros: ', ml)
