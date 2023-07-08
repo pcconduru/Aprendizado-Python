963ea46385db2f38961bd1cb30dac19da67742fd
Paulo César Conduru <133069658+pcconduru@users.noreply.github.com>
2023-07-08 12:32:24 -0300

    Update desafio-04.py

--- Desafios/Aula-6/desafio-04.py
+++ Desafios/Aula-6/desafio-04.py
@@ -1,3 +1,4 @@
 writed = input('Digite algo:')
-print('Tipo: {}, Alpha: {}, Númerico: {}, AlphaNúmerico: {}'.format(type(writed), writed.isalpha(), writed.isnumeric(), writed.isalnum()))
-
+print('Tipo: {}, Alpha: {}, Númerico: {}, AlphaNúmerico: {}'
+      .format(type(writed), writed.isalpha(), writed.isnumeric(),
+                                                                    writed.isalnum()))
