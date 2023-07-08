2b968534695254229dd0b26131f557b85e18ffbd
Paulo César Conduru <133069658+pcconduru@users.noreply.github.com>
2023-07-08 13:04:03 -0300

    Create ex004.py

---
+++ Exercicios/ex004.py
@@ -0,0 +1,8 @@
+x = input('Digite algo:')
+print('Tipo primitivo:', type(x))
+print('É um númerico:', x.isnumeric())
+print('É um alpha:', x.isalpha())
+print('É um AlphaNúmerico:', x.isalnum())
+print('É maiúscula:', x.isupper())
+print('É minúscula:', x.islower())
+print('É capitalizado:', x.istitle())
