453299074b4bcb8e15700b7fda4006ad60cd8082
Paulo César Conduru <133069658+pcconduru@users.noreply.github.com>
2023-07-14 18:13:45 -0300

    Create ex038.py

---
+++ Exercicios/ex038.py
@@ -0,0 +1,9 @@
+#Analisando um valor
+v1 = int(input('Primeiro valor: '))
+v2 = int(input('Segundo valor: '))
+if v1 > v2:
+    print('O primeiro valor é MAIOR.')
+elif v1 < v2:
+    print('O segundo valor é MAIOR')
+else:
+    print('Os valores são iguais.')
